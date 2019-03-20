6ec759a4536ca682ab098e6d37081dc99cdea74c
Zellat Abdelkhalek <ha_zellat@esi.dz>
2019-03-20 02:35:26 +0100

    Add Main Sections

---
+++ resources/views/sections/about.blade.php
@@ -0,0 +1,38 @@
+
+
+  <div class="about" id="about" style="background-color: #FBFBFB;">
+        <div class="about-fluid">
+          <div class="row about-row">
+            <div class="col-md-6 col-lg-8 order-2 order-md-1">
+              <div class="row about-text">
+                <div class="col-12 about-title">
+                  What is HackIT
+                </div>
+    
+                <div class="col-12 about-contents">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus condimentum commodo dui, sit amet
+                  tempus lacus aliquam sit amet. Ut vel finibus lacus, in dictum est. Pellentesque pulvinar tincidunt enim.
+                  Vivamus molestie laoreet ante quis vehicula. Proin at ante sit amet metus tincidunt bibendum a et diam.
+                  Aliquam pretium neque vitae mi condimentum, eget ornare ligula facilisis. Nunc cursus, est vitae ultricies
+                  aliquet, turpis magna blandit nibh, id ultrices elit mi vel libero. Donec sed dolor sed est varius
+                  vulputate in quis sapien. Sed facilisis lacinia commodo.
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4 order-1 order-md-2">
+              <img src="images/isometric.svg" class="about-img">
+            </div>
+    
+          </div>
+    
+          <div class="row">
+            <div class="col-12">
+              <iframe width="560" class="about-video" height="450" src="https://www.youtube.com/embed/VCusixl9H18"
+                frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+    
---
+++ resources/views/sections/landing.blade.php
@@ -0,0 +1,38 @@
+
+  <div class="landing-page">
+        <div class="container-fluid landing-contents">
+          <div class="row">
+            <div class="col-12">
+              <img src="images/LOGO.png" class="logo-img">
+    
+            </div>
+          </div>
+    
+          <div class="row" style="margin-top: 4%;">
+            <div id="gallery-btn" class="col-md-6 landing-contents-button">
+              <a href="{{route('register')}}" target="_blank"><button type="button"
+                  class="btn btn-outline-primary landing-nav-btn">Register</button></a>
+            </div>
+    
+    
+    
+            <div id="sponsor-btn" class="col-md-6 landing-contents-button">
+              <a href="#contact"><button type="button" class="btn btn-outline-danger landing-nav-btn"
+                  style="border-color:#EBDE0D; color:#EBDE0D;">Contact Us</button></a>
+            </div>
+          </div>
+    
+    
+    
+          <div class="row justify-content-center text-center landing-icons"
+            style="margin-top: 4%; padding-left: 8%; padding-right: 8%; transition: all 0.6s;">
+    
+            <div class="col-6 col-md-6 landing-info" style="display:inline-block">
+              <img src="images/Group%2034.svg" class="pointer-svg"> &nbsp ESI - Alger
+            </div>
+            <div class="col-6 col-md-6 landing-info">
+              <img src="images/calendar.svg" class="calendar-svg"> &nbsp 29th - 31st Avril
+            </div>
+          </div>
+        </div>
+      </div>
