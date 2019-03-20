7441f2b9d42077d69c8a82a6f669e1361bda604e
Zellat Abdelkhalek <ha_zellat@esi.dz>
2019-03-20 02:45:29 +0100

    Add Comments , Refactor Code

--- resources/views/mainPage.blade.php
+++ resources/views/mainPage.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.default') 
 @section('styles')
-<link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
-<link rel="stylesheet" href="css/style.css">
+
+<!-- CSS Links -->
+<link rel="stylesheet" href="{{asset('css/bootstrap/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/style.css')}}">
+<!-- Fonts Links -->
 <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=K2D" rel="stylesheet">
-<link rel="stylesheet" href="../use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
-    crossorigin="anonymous">
+<link rel="stylesheet" href="../use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
 @endsection
- 
+ <!-- Main Page -->
 @section('content')
 
     @include('layouts.navbar')
@@ -23,10 +26,10 @@
 
 @endsection
 
-
+<!-- Scripts Section -->
 @section('scripts')
 
 <script src="../code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-<script src="js/bootstrap/bootstrap.min.js"></script>
-<script src="js/script.js"></script>
+<script src="{{asset('js/bootstrap/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/script.js')}}"></script>
 @endsection
