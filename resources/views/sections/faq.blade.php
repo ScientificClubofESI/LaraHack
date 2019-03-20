3b0268935ed6a7b7964072718d3ae7f914c66101
Zellat Abdelkhalek <ha_zellat@esi.dz>
2019-03-20 02:35:55 +0100

    Add FAQ , Sponsors Sections

---
+++ resources/views/sections/faq.blade.php
@@ -0,0 +1,98 @@
+
+
+  <div class="faq" id="faq">
+        <div class="container-fluid">
+          <div class="row" style="margin-bottom: 40px;">
+            <div class="col-12 speaker-title">
+              FAQ
+            </div>
+          </div>
+          <div class="row faq-row">
+    
+    
+            <div class="col-md-6">
+              <div class="faq-collapsible">
+                <button class="collapsible">Lorem ipsum dolor sit amet, consectetur ?</button>
+                <div class="content">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ultrices dui finibus porta maximus.
+                    Donec eget dignissim sapien, eget interdum nunc. Etiam pretium
+                    .
+                  </p>
+                </div>
+              </div>
+              <div class="faq-collapsible">
+                <button class="collapsible">Lorem ipsum dolor sit amet, consectetur ?</button>
+                <div class="content">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ultrices dui finibus porta maximus.
+                    Donec eget dignissim sapien, eget interdum nunc. Etiam pretium
+                    .
+                  </p>
+                </div>
+              </div>
+              <div class="faq-collapsible">
+                <button class="collapsible">Lorem ipsum dolor sit amet, consectetur ?</button>
+                <div class="content">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ultrices dui finibus porta maximus.
+                    Donec eget dignissim sapien, eget interdum nunc. Etiam pretium
+                    .
+                  </p>
+                </div>
+              </div>
+              <div class="faq-collapsible">
+                <button class="collapsible">Lorem ipsum dolor sit amet, consectetur ?</button>
+                <div class="content">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ultrices dui finibus porta maximus.
+                    Donec eget dignissim sapien, eget interdum nunc. Etiam pretium
+                    .
+                  </p>
+                </div>
+              </div>
+    
+            </div>
+            <div class="col-md-6">
+              <div class="faq-collapsible">
+                <button class="collapsible">Lorem ipsum dolor sit amet, consectetur ?</button>
+                <div class="content">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ultrices dui finibus porta maximus.
+                    Donec eget dignissim sapien, eget interdum nunc. Etiam pretium
+                    .
+                  </p>
+                </div>
+              </div>
+              <div class="faq-collapsible">
+                <button class="collapsible">Lorem ipsum dolor sit amet, consectetur ?</button>
+                <div class="content">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ultrices dui finibus porta maximus.
+                    Donec eget dignissim sapien, eget interdum nunc. Etiam pretium
+                    .
+                  </p>
+                </div>
+              </div>
+              <div class="faq-collapsible">
+                <button class="collapsible">Lorem ipsum dolor sit amet, consectetur ?</button>
+                <div class="content">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ultrices dui finibus porta maximus.
+                    Donec eget dignissim sapien, eget interdum nunc. Etiam pretium
+                    .
+                  </p>
+                </div>
+              </div>
+              <div class="faq-collapsible">
+                <button class="collapsible">Lorem ipsum dolor sit amet, consectetur ?</button>
+                <div class="content">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ultrices dui finibus porta maximus.
+                    Donec eget dignissim sapien, eget interdum nunc. Etiam pretium
+                    .
+                  </p>
+                </div>
+              </div>
+    
+            </div>
+    
+          </div>
+        </div>
+      </div>
+    
+    
+    
+    
---
+++ resources/views/sections/sponsors.blade.php
@@ -0,0 +1,80 @@
+
+
+ 
+
+
+
+  <div class="container-fluid sponsors" id="sponsor-target">
+        <div class="row">
+          <div class="col-12 text-center sponsor-title">
+            Sponsors
+          </div>
+        </div>
+    
+        <div class="row justify-content-around main-sponsors" style="margin-bottom: 10%;">
+          <div class="col-lg-6 image-column">
+            <a href="https://github.com/" target="_blank"><img src="images/sponsors/github.png" style="width:80%;"
+                class="title-sponsor-image"></a>
+          </div>
+    
+          <div class="col-lg-6 image-column">
+            <a href="https://www.cse.club/" target="_blank"><img src="images/sponsors/cse.png" style="width:80%;"
+                class="title-sponsor-image"></a>
+          </div>
+    
+    
+        </div>
+    
+    
+        <div class="row patron-sponsors">
+          <div class="col-12 text-center patron-title">
+            Partners
+          </div>
+        </div>
+    
+        <div class="row justify-content-center" style="margin-bottom: 10%;">
+    
+          <div class="col-12 col-md-6 col-lg-3 image-column">
+            <a href="https://www.producthunt.com/" target="_blank"><img src="images/sponsors/producthunt.png"
+                class="sponsor-image" style="width:80%;"></a>
+          </div>
+    
+          <div class="col-12 col-md-6 col-lg-3 image-column">
+            <a href="https://www.sketchapp.com/" target="_blank"><img src="images/sponsors/sketch.png" class="sponsor-image"
+                style="width:70%;"></a>
+          </div>
+    
+          <div class="col-12 col-md-6 col-lg-3 image-column">
+            <a href="http://www.bitalino.com/" target="_blank"><img
+                src="images/sponsors/bitalino_logo_2.0_falpha-00.png" class="sponsor-image"></a>
+          </div>
+    
+          <div class="col-12 col-md-6 col-lg-3 image-column">
+            <a href="https://www.plux.info/" target="_blank"><img src="images/sponsors/logo_bw.png"
+                class="sponsor-image"></a>
+          </div>
+    
+          <div class="col-12 col-md-6 col-lg-3 image-column">
+            <a href="http://advantedge.vc/" target="_blank"><img src="images/sponsors/AdvantEdge.png" class="sponsor-image"
+                style="width:40%"></a>
+          </div>
+    
+          <div class="col-12 col-md-6 col-lg-3 image-column">
+            <a href="https://pentesterlab.com/" target="_blank"><img src="images/sponsors/pentesrlab.png"
+                class="sponsor-image"></a>
+          </div>
+    
+          <div class="col-12 col-md-6 col-lg-3 image-column">
+            <a href="https://www.creative-tim.com/" target="_blank"><img src="images/sponsors/tim.png"
+                class="sponsor-image"></a>
+          </div>
+    
+          <div class="col-12 col-md-6 col-lg-3 image-column">
+            <a href="https://www.stickeryou.com/" target="_blank"><img src="images/sponsors/SY.png"
+                class="sponsor-image"></a>
+          </div>
+        </div>
+      </div>
+    
+      
+    
