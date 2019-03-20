7cc35fddeddb3ae8482a304b8958d3b6255c008b
Zellat Abdelkhalek <ha_zellat@esi.dz>
2019-03-20 02:35:44 +0100

    Add Challenges , Prizes Section

---
+++ resources/views/sections/challenges.blade.php
@@ -0,0 +1,51 @@
+<div id="challenges" class="container-fluid mentors" style="background-color: #FBFBFB;">
+        <div class="row">
+          <div class="col-12 speaker-title">
+            Challenges
+          </div>
+        </div>
+    
+    
+        <div class="row">
+          <div class="col-12">
+            <div class="mentor-container">
+              <div class="row">
+                <div class="col-md-4 col-12">
+                  <img src="images/themes/smartCity.jpg" class="mentor-img">
+                </div>
+                <div class="col-md-8 col-12 text-center mentor-text">
+                  <div class="mentor-title"><span class="mentor-name">Smart Cities</span><br></div>
+                  <div class="mentor-info">
+                    Nulla eleifend vitae nunc sed lobortis. Quisque elementum nunc non neque volutpat pulvinar. Aenean
+                    viverra nibh dui, quis molestie ante commodo non. Nam mollis in lorem rhoncus faucibus. Aenean et
+                    faucibus tortor. Proin a elit suscipit, tempor urna a, condimentum elit. Praesent varius cursus mi at
+                    sagittis. Vivamus suscipit felis eget gravida sodales. Donec lacinia diam ante, sit amet cursus nibh
+                    tempus ut. Vivamus eu ultricies enim, vitae egestas tellus. </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12">
+            <div class="mentor-container">
+              <div class="row">
+                <div class="col-md-4 col-12">
+                  <img src="images/themes/Student.jpg" class="mentor-img">
+                </div>
+                <div class="col-md-8 col-12 text-center mentor-text">
+                  <div class="mentor-title"><span class="mentor-name">Student Life</span><br></div>
+                  <div class="mentor-info">
+                    Nulla eleifend vitae nunc sed lobortis. Quisque elementum nunc non neque volutpat pulvinar. Aenean
+                    viverra nibh dui, quis molestie ante commodo non. Nam mollis in lorem rhoncus faucibus. Aenean et
+                    faucibus tortor. Proin a elit suscipit, tempor urna a, condimentum elit. Praesent varius cursus mi at
+                    sagittis. Vivamus suscipit felis eget gravida sodales. Donec lacinia diam ante, sit amet cursus nibh
+                    tempus ut. Vivamus eu ultricies enim, vitae egestas tellus. </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    
+    
+    
+        </div>
+      </div>
+    
---
+++ resources/views/sections/prizes.blade.php
@@ -0,0 +1,58 @@
+
+
+  <div class="container prizes">
+        <div class="row" style="margin-bottom: 40px;">
+          <div class="col-12 speaker-title">
+            Prizes and Rewards
+          </div>
+        </div>
+        <div class="row prizes-row justify-content-around">
+          <div class="col-sm-12 col-lg-4 col-md-6 col-12">
+            <div class="container prize-container top-prize">
+              <div class="image-row">
+                <div class="image-text">
+                  $30,000
+                </div>
+              </div>
+    
+              <div class="details-row text-center">
+                <strong>1st Prize</strong>
+              </div>
+            </div>
+          </div>
+    
+          <div class="col-sm-12 col-lg-4 col-md-6 col-12">
+            <div class="container prize-container top-prize">
+              <div class="image-row">
+                <div class="image-text">
+                  $20,000
+                </div>
+              </div>
+    
+              <div class="details-row text-center">
+                <strong>2nd Prize</strong>
+              </div>
+            </div>
+          </div>
+    
+          <div class="col-sm-12 col-lg-4 col-md-6 col-12">
+            <div class="container prize-container top-prize">
+              <div class="image-row">
+                <div class="image-text">
+                  $10,000
+                </div>
+    
+              </div>
+    
+              <div class="details-row text-center">
+                <strong>3rd Prize</strong>
+              </div>
+            </div>
+          </div>
+        </div>
+    
+    
+    
+    
+      </div>
+    
