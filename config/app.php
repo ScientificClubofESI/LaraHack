e724607036872cfb979a8868adee018a26f69a3f
Zellat Abdelkhalek <ha_zellat@esi.dz>
2019-03-25 04:07:20 +0100

    Add CSS Mail Inline Config

--- config/app.php
+++ config/app.php
@@ -162,6 +162,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Fedeisas\LaravelMailCssInliner\LaravelMailCssInlinerServiceProvider::class,
+
 
         /*
          * Package Service Providers...
