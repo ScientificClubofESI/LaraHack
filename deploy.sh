94ef90fe68d4b12366de3be97c70426dda336155
Oussama Messabih <thehappybit@sam.localdomain>
2020-01-01 20:23:19 +0100

    updating docker config

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 php artisan key:generate
 php artisan config:cache
+php artisan storage:link
 # check if the database is ready
 echo "Waiting for Mysql DB"
 while ! mysql --protocol TCP -u"homestead" -p"homestead" -h"db" -e "show databases;" > /dev/null 2>&1;
